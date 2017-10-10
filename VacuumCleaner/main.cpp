297a42754b0ad40442ab920ca0f14708ea6233b1
Baneo <aurelien.durance@utbm.fr>
2017-10-10 16:24:37 -0400

    namespace

--- VacuumCleaner/main.cpp
+++ VacuumCleaner/main.cpp
@@ -3,13 +3,15 @@
 #include "environment.h"
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     //w.show();
     Environment e;
-    std::cout<<"running e";
+    cout << "running e";
     e.run();
 
     return a.exec();
